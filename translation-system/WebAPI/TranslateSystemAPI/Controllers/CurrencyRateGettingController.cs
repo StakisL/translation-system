4d5e402c9dc01a40dc7e2802c7af09aed2b40168
Stanislav <lebel@glavsoft.com>
2021-03-18 19:56:32 +0700

    Move database project in right folders. Add first controller.

---
+++ translation-system/WebAPI/TranslateSystemAPI/Controllers/CurrencyRateGettingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TranslateSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyRateGettingController : ControllerBase
+    {
+        public CurrencyRateGettingController()
+        {
+        }
+
+        [HttpGet]
+        //todo Добавить модель для возвращения нужного типа!
+        public void GetCurrentExchangeCurrencyRate()
+        {
+        }
+    }
+}
