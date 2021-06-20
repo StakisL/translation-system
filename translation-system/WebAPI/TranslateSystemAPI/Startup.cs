906ef4404e2cbbae36e58e44f35d0ec7ef0a6662
Stanislav <lebel@glavsoft.com>
2021-06-20 19:11:24 +0700

    Add cron expression, register job services.

--- translation-system/WebAPI/TranslateSystemAPI/Startup.cs
+++ translation-system/WebAPI/TranslateSystemAPI/Startup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -6,10 +7,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
 using Serilog;
 using TranslateSystem.Persistence;
 using TranslateSystem.Persistence.Postgre;
 using TranslateSystem.Persistence.Settings;
+using TranslateSystemAPI.Scheduler;
+using TranslateSystemAPI.Scheduler.Jobs;
 
 namespace TranslateSystemAPI
 {
@@ -31,6 +37,9 @@ namespace TranslateSystemAPI
                 new BasicConnectionProvider(Configuration["ConnectionString"]));
 
             services.AddScoped(sp => sp.GetRequiredService<IContextFactory>().CreateContext());
+            
+            ConfigureScheduler(services);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TranslateSystemAPI", Version = "v1" });
@@ -79,6 +88,32 @@ namespace TranslateSystemAPI
 
             context?.Database.Migrate();
             Log.Information($"DB migration completed");
+        }
+
+        private void ConfigureScheduler(IServiceCollection services)
+        {
+            services.AddSingleton<IJobFactory, JobFactory>();
+            services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+            services.AddSingleton<JobRunner>();
+            services.AddScoped<ExchangeRateRequestJob>();
+
+            var jobs = SetJobSchedule();
+            foreach (var job in jobs)
+            {
+                services.AddSingleton(job);
+            }
+            
+            services.AddHostedService<SchedulerHostedService>();
+        }
+        
+        private ICollection<JobSchedule> SetJobSchedule()
+        {
+            var configSection = Configuration.GetSection("Scheduler");
+            var schedulerJobs = configSection.GetChildren()
+                .Select(cs => new JobSchedule(
+                    jobType: cs.Key.CurrentJob(),
+                    cronExpression: cs.Value)).ToList();
+            return schedulerJobs;
         }
     }
 }
